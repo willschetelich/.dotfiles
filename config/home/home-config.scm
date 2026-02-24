08381919d7ab0a298608ea588cc8a4eb8305719a
will <willschetelich@gmail.com>
2026-02-24 12:53:40 -0500

    move system and home config

---
+++ config/home/home-config.scm
@@ -0,0 +1,26 @@
+(use-modules (gnu)
+	     (gnu home)
+	     (nongnu packages chrome)
+	     (nongnu packages productivity)
+	     (gnu home services ssh)
+	     )
+	  
+
+(home-environment
+ (packages (specifications->packages
+	    (list "git"
+		  "emacs"
+		  "obsidian"
+		  "google-chrome-stable"
+		  "openssh"
+		  )))
+ (services (list (service home-openssh-service-type
+			  (home-openssh-configuration
+			   (add-keys-to-agent "yes")
+			   (hosts
+			    (list (openssh-host (name "github.com")
+						(user "git")
+						(identity-file "~/.ssh/id_ed25519")
+					
+						)))))))
+)
---
+++ config/systems/system.scm
@@ -0,0 +1,76 @@
+;; This is an operating system configuration generated
+;; by the graphical installer.
+;;
+;; Once installation is complete, you can learn and modify
+;; this file to tweak the system configuration, and pass it
+;; to the 'guix system reconfigure' command to effect your
+;; changes.
+
+
+;; Indicate which modules to import to access the variables
+;; used in this configuration.
+(use-modules (gnu) (nongnu packages linux))
+(use-service-modules cups desktop networking ssh xorg)
+
+(operating-system
+ (kernel linux)
+ (firmware (list linux-firmware))
+  (locale "en_US.utf8")
+  (timezone "America/New_York")
+  (keyboard-layout (keyboard-layout "us"))
+  (host-name "will-guix")
+
+  ;; The list of user accounts ('root' is implicit).
+  (users (cons* (user-account
+                  (name "will")
+                  (comment "will")
+                  (group "users")
+                  (home-directory "/home/will")
+                  (supplementary-groups '("wheel" "netdev" "audio" "video")))
+                %base-user-accounts))
+
+  ;; Packages installed system-wide.  Users can also install packages
+  ;; under their own account: use 'guix search KEYWORD' to search
+  ;; for packages and 'guix install PACKAGE' to install a package.
+  (packages (append (list (specification->package "i3-wm")
+                          (specification->package "i3status")
+                          (specification->package "dmenu")
+                          (specification->package "st")
+                          (specification->package "emacs")
+                          (specification->package "emacs-exwm")
+                          (specification->package
+                           "emacs-desktop-environment")) %base-packages))
+
+  ;; Below is the list of system services.  To search for available
+  ;; services, run 'guix system search KEYWORD' in a terminal.
+  (services
+   (append (list (service xfce-desktop-service-type)
+                 (set-xorg-configuration
+                  (xorg-configuration (keyboard-layout keyboard-layout))))
+
+           ;; This is the default list of services we
+           ;; are appending to.
+           %desktop-services))
+  (bootloader (bootloader-configuration
+                (bootloader grub-efi-bootloader)
+                (targets (list "/boot/efi"))
+                (keyboard-layout keyboard-layout)))
+  (mapped-devices (list (mapped-device
+                          (source (uuid
+                                   "f6fc7538-edb6-4c4f-aab6-fb150d441f79"))
+                          (target "cryptroot")
+                          (type luks-device-mapping))))
+
+  ;; The list of file systems that get "mounted".  The unique
+  ;; file system identifiers there ("UUIDs") can be obtained
+  ;; by running 'blkid' in a terminal.
+  (file-systems (cons* (file-system
+                         (mount-point "/boot/efi")
+                         (device (uuid "519E-A5CB"
+                                       'fat32))
+                         (type "vfat"))
+                       (file-system
+                         (mount-point "/")
+                         (device "/dev/mapper/cryptroot")
+                         (type "ext4")
+                         (dependencies mapped-devices)) %base-file-systems)))
