d79c204c4e4806ef27518864cca8f8be01c639cf
will <willschetelich@gmail.com>
2026-02-25 19:55:54 -0500

    factored out home-config sorta

---
+++ config/home/services/ssh-service-config.scm
@@ -0,0 +1,14 @@
+(define-module (config home services ssh-service-config)
+  #:use-module (gnu services)
+  #:use-module (gnu home services ssh)
+  #:export (home-open-ssh-service-type))
+
+  (define home-openssh-service
+    (service home-openssh-service-type
+			  (home-openssh-configuration
+			   (add-keys-to-agent "yes")
+			   (hosts
+			    (list (openssh-host (name "github.com")
+						(user "git")
+						(identity-file "~/.ssh/id_ed25519")
+						)))))
