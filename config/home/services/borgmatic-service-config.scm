8a80660c31144e792174f871f48890a252e73792
will <willschetelich@gmail.com>
2026-02-25 20:49:36 -0500

    add guix gexp to borgmatic module

--- config/home/services/borgmatic-service-config.scm
+++ config/home/services/borgmatic-service-config.scm
@@ -1,6 +1,17 @@
 (define-module (config home services borgmatic-service-config)
   #:use-module (gnu home services)
   #:use-module (gnu services)
+  #:use-module (guix gexp)
+
+  #:export (borgmatic-files-service))
+
+(define borgmatic-files-service
+  (service home-files-service-type
+			  `((".config/borgmatic/config.yaml"
+			     ,(local-file "/home/will/.dotfiles/files/borgmatic-config.yaml")))))
+(define-module (config home services borgmatic-service-config)
+  #:use-module (gnu home services)
+  #:use-module (gnu services)
   #:export (borgmatic-files-service))
 
 (define borgmatic-files-service
