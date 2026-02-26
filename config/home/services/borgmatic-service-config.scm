a1409892133596c09757789353782d9d1e120496
will <willschetelich@gmail.com>
2026-02-25 21:03:43 -0500

    removed duplicate code of borgmatic module for real this time

--- config/home/services/borgmatic-service-config.scm
+++ config/home/services/borgmatic-service-config.scm
@@ -9,12 +9,3 @@
   (service home-files-service-type
 			  `((".config/borgmatic/config.yaml"
 			     ,(local-file "/home/will/.dotfiles/files/borgmatic-config.yaml")))))
-(define-module (config home services borgmatic-service-config)
-  #:use-module (gnu home services)
-  #:use-module (gnu services)
-  #:export (borgmatic-files-service))
-
-(define borgmatic-files-service
-  (service home-files-service-type
-			  `((".config/borgmatic/config.yaml"
-			     ,(local-file "/home/will/.dotfiles/files/borgmatic-config.yaml")))))
---
+++ config/home/services/shell-services-config.scm
@@ -0,0 +1,6 @@
+(define-module (config home services shell-services-config)
+  #:use-module (gnu home)
+  #:use-module (gnu home services sells)
+  )
+
+(define
