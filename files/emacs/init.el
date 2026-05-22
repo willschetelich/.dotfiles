eb2da95d680d728d1a78bea80cf98cb7685940e6
will <willschetelich@gmail.com>
2026-05-22 18:12:56 -0400

    update init.el

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -1,30 +1,29 @@
-;; Melpha
+;; MELPA Package Manager
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; turn off noisy file generation 
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-(setq create-lockfiles nil)
+;; Install use-package to manage configuration cleanly
+(unless (package-installed-p 'use-package)
+  (package-refresh-packages)
+  (package-install 'use-package))
+(eval-when-compile (require 'use-package))
 
+;; Turn off noisy file generation 
+(setq make-backup-files nil) ;; this gets rid of the '~' files
+(setq auto-save-default nil) ;; this disables the # files
+(setq create-lockfiles nil)  ;; disables .# in files
 
+;; Modern Auto-Save (Saves the REAL file when you switch focus/stop typing)
+(use-package super-save
+  :ensure t
+  :config
+  (super-save-mode +1)
+  (setq super-save-auto-save-when-idle t
+        super-save-idle-duration 3)) ;; Saves after 3 seconds of idle time
 
-;; in Org Babel
-;; Load the language
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((python . t)))
-
-;; Point to the correct executable
-(setq org-babel-python-command "python3")
-
-;; Prevent Emacs from creating a separate .emacs.d for internal storage
-(setq user-emacs-directory "~/.config/emacs/")
-
-(setq org-agenda-files '("~/org/"))
-
-(setq org-todo-keywords
-      '((sequence "TODO" "IN-PROGRESS" "MUSIC-TODO" | "DONE" "CANCELLED")))
-
-(setq org-return-follows-link nil)
+;; Essential UI Cleanup
+(setq inhibit-startup-message t) ;; Skip the splash screen
+(menu-bar-mode -1)               ;; Hide the top menu bar
+(tool-bar-mode -1)               ;; Hide the top icon toolbar
+(scroll-bar-mode -1)             ;; Hide the scrollbar for maximum screen space
